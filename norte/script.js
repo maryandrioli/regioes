7c4492d4d5f5cff01dee293fc695f865cf2a7224
maryandrioli <maryandrioli@gmail.com>
2023-12-10 09:14:19 -0300

    Update script.js

--- norte/script.js
+++ norte/script.js
@@ -37,6 +37,10 @@ function handleKeyPress(event) {
             return; // No need to play sound or change size
     }
 
+    playSoundAndAnimate(circle, sound);
+}
+
+function playSoundAndAnimate(circle, sound) {
     if (!concurrentPlay) {
         stopAllSounds();
     }
@@ -74,5 +78,13 @@ document.addEventListener('keyup', () => {
     document.querySelectorAll('.circle').forEach(circle => {
         circle.style.width = '';
         circle.style.height = '';
+    });
+});
+
+// Add click event listeners to circles
+document.querySelectorAll('.circle').forEach(circle => {
+    circle.addEventListener('click', () => {
+        let sound = sounds[circle.id];
+        playSoundAndAnimate(circle, sound);
     });
 });

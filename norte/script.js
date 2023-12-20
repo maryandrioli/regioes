e32b30380dc4e2564340597d91b6935a205b9dce
maryandrioli <maryandrioli@gmail.com>
2023-12-20 07:05:35 -0300

    script

--- norte/script.js
+++ norte/script.js
@@ -4,40 +4,63 @@ let sounds = {
     red: new Audio('sons/CHUVA.wav'),
     yellow: new Audio('sons/GRILOS.wav'),
     blue: new Audio('sons/RIOAMAZONAS.wav'),
-    orange: new Audio('sons/CARIMBO.wav')
+    orange: new Audio('sons/CARIMBO.wav'),
+    notado: new Audio('sons/notado.mp3'),
+    re: new Audio('sons/re.mp3'),
+    mi: new Audio('sons/mi.mp3'),
+    fa: new Audio('sons/fa.mp3'),
+    sol: new Audio('sons/sol.mp3')
 };
-
 let concurrentPlay = false;
 
 function handleKeyPress(event) {
-    let circle, sound;
+    let circle, sound, additionalSound;
     switch (event.key) {
         case 'ArrowUp':
             circle = document.getElementById('green');
+            additionalSound = sounds.notado;
             sound = sounds.green;
             break;
         case 'ArrowDown':
             circle = document.getElementById('red');
+            additionalSound = sounds.re;
             sound = sounds.red;
             break;
         case 'ArrowLeft':
             circle = document.getElementById('yellow');
+            additionalSound = sounds.mi;
             sound = sounds.yellow;
             break;
         case 'ArrowRight':
             circle = document.getElementById('blue');
+            additionalSound = sounds.fa;
             sound = sounds.blue;
             break;
         case ' ':
             circle = document.getElementById('orange');
+            additionalSound = sounds.sol;
             sound = sounds.orange;
             break;
         case 'Enter':
             toggleConcurrentPlay();
             return; // No need to play sound or change size
     }
+    playAdditionalSound(circle, additionalSound, sound);
+}
 
-    playSoundAndAnimate(circle, sound);
+function playAdditionalSound(circle, additionalSound, mainSound) {
+    if (!concurrentPlay) {
+        stopAllSounds();
+    }
+
+    if (additionalSound) {
+        additionalSound.play();
+        setTimeout(() => {
+            playSoundAndAnimate(circle, mainSound);
+        }, 1000); // Ajuste esse tempo de acordo com a duração de cada som adicional
+    } else {
+        playSoundAndAnimate(circle, mainSound);
+    }
 }
 
 function playSoundAndAnimate(circle, sound) {
